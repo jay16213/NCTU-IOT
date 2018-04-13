00239baba7d1630a58b1e8d907fa6c87d30cc46e
Jay Lin <jay101630@gmail.com>
2018-04-13 14:39:33 +0800

    my device

--- Dummy_Device/DAI.py
+++ Dummy_Device/DAI.py
@@ -1,32 +1,36 @@
 import time, DAN, requests, random
 
 ServerIP = '140.113.199.204' #Change to your IoTtalk IP or None for autoSearching
-Reg_addr=None #None # if None, Reg_addr = MAC address
+Reg_addr='0416213' #None # if None, Reg_addr = MAC address
 
-DAN.profile['dm_name']='Dummy_Device'
-DAN.profile['df_list']=['Dummy_Sensor', 'Dummy_Control','OneParameter','ThreeParameter']
+DAN.profile['dm_name']='0416213'
+DAN.profile['df_list']=['0416213_DF']
 DAN.profile['d_name']= None # None for autoNaming
 DAN.device_registration_with_retry(ServerIP, Reg_addr)
 
-switch = 0
+last_state = 0
+state = 0
 
 while True:
     try:
         # Pull data from a device feature called "Dummy_Control"
-        value1=DAN.pull('Dummy_Control')
-        if value1 != None:
-                #print(type(value1))
-                #print(len(value1))
-            if value1[0] != None:
-                # print(value1[0][0])
-                # print(value1[0][1])
-                if value1[0][0] > 0:
-                    switch = 1
-                else:
-                    switch = 0
+        value=DAN.pull('0416213_DF')
+        if value != None:
+            #print(value[0])
+            if value[0] >= 0:
+                last_state = state
+                state = 1
+            else:
+                last_state = state
+                state = 0
 
-            if switch == 1:
-                print("x: {}, y: {}, z: {}".format(value1[0][1][0], value1[0][1][1], value1[0][1][2]))
+            if last_state != state:
+                if state == 1:
+                    print("smart phone is up")
+                else:
+                    print("smart phone is down")
+            # if switch == 1:
+            #     print("x: {}, y: {}, z: {}".format(value1[0][1][0], value1[0][1][1], value1[0][1][2]))
 
     except Exception as e:
         print(e)
