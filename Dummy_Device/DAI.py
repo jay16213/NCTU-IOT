7d22bb391eca0d5afe4f3aae91afb9c6b5942a0e
Jay Lin <jay101630@gmail.com>
2018-04-13 13:36:51 +0800

    try

--- Dummy_Device/DAI.py
+++ Dummy_Device/DAI.py
@@ -11,25 +11,25 @@ DAN.device_registration_with_retry(ServerIP, Reg_addr)
 switch = 0
 
 while True:
-    # try:
-    #Pull data from a device feature called "Dummy_Control"
-    value1=DAN.pull('Dummy_Control')
-    if value1 != None:
-            #print(type(value1))
-            #print(len(value1))
-        if value1[0] != None:
-            # print(value1[0][0])
-            # print(value1[0][1])
-            if value1[0][0] > 0:
-                switch = 1
-            else:
-                switch = 0
+    try:
+        # Pull data from a device feature called "Dummy_Control"
+        value1=DAN.pull('Dummy_Control')
+        if value1 != None:
+                #print(type(value1))
+                #print(len(value1))
+            if value1[0] != None:
+                # print(value1[0][0])
+                # print(value1[0][1])
+                if value1[0][0] > 0:
+                    switch = 1
+                else:
+                    switch = 0
 
-        if switch == 1:
-            print("x: {}, y: {}, z: {}".format(value1[0][1][0], value1[0][1][1], value1[0][1][2]))
+            if switch == 1:
+                print("x: {}, y: {}, z: {}".format(value1[0][1][0], value1[0][1][1], value1[0][1][2]))
 
-    # except Exception as e:
-    #     print(e)
-    #     DAN.device_registration_with_retry(ServerIP, Reg_addr)
+    except Exception as e:
+        print(e)
+        DAN.device_registration_with_retry(ServerIP, Reg_addr)
 
     time.sleep(0.2)
